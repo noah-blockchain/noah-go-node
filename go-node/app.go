520d01d4004bca73357a087867c09e2a68095d4c
Ivan Borisov <2622610@gmail.com>
2019-07-18 01:19:36 +0300

    added high-level architecture

---
+++ go-node/app.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+
+	"github.com/cosmos/cosmos-sdk/x/auth/genaccounts"
+
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	distr "github.com/cosmos/cosmos-sdk/x/distribution"
+	"github.com/cosmos/cosmos-sdk/x/params"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	"github.com/cosmos/sdk-application-tutorial/x/nameservice"
+
+	bam "github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	dbm "github.com/tendermint/tendermint/libs/db"
+)
+
+const appName = "nameservice"
+
+var (
+	// default home directories for the application CLI
+	DefaultCLIHome = os.ExpandEnv("$HOME/.nscli")
+
+	// DefaultNodeHome sets the folder where the application data and configuration will be stored
+	DefaultNodeHome = os.ExpandEnv("$HOME/.nsd")
+
+	// ModuleBasicManager is in charge of setting up basic module elements
+	ModuleBasics sdk.ModuleBasicManager
+)
+
+type nameServiceApp struct {
+	*bam.BaseApp
+}
