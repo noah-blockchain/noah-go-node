7d15ae713864b87430ce4dd98decaa557ae1fc16
Ivan Borisov <2622610@gmail.com>
2019-08-23 22:43:39 +0300

    fix

--- version/version.go
+++ version/version.go
@@ -2,16 +2,16 @@ package version
 
 // Version components
 const (
-	Maj = "1"
-	Min = "0"
-	Fix = "4"
+	Maj = "0"
+	Min = "1"
+	Fix = "0"
 
-	AppVer = 5
+	AppVer = 8
 )
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "1.0.4"
+	Version = "0.1.0"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
