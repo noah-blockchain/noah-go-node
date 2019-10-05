933b1dafae59965787345f7486a762152a1c8009
Noah GO Developer <developer.chain.2019@hotmail.com>
2019-10-05 09:10:57 +0300

    fix constants

--- core/types/constants.go
+++ core/types/constants.go
@@ -6,7 +6,7 @@ const (
 	ChainTestnet = 0x02
 	ChainMainnet = 0x01
 
-	CurrentChainID = ChainTestnet
+	CurrentChainID = ChainMainnet
 )
 
 func GetBaseCoin() CoinSymbol {
@@ -20,7 +20,7 @@ func getBaseCoin(chainID ChainID) CoinSymbol {
 	case ChainMainnet:
 		copy(coin[:], []byte("NOAH"))
 	case ChainTestnet:
-		copy(coin[:], []byte("NOAH")) // todo
+		copy(coin[:], []byte("DAO")) // todo
 	}
 
 	coin[4] = byte(0)
