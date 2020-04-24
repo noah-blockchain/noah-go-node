f47ed2968c710025bf43671b54fd11955c1fe679
truego <truego+noah@gmail.com>
2020-04-25 01:22:43 +0900

    fix version number

--- version/version.go
+++ version/version.go
@@ -4,14 +4,14 @@ package version
 const (
 	Maj = "0"
 	Min = "2"
-	Fix = "0"
+	Fix = "5"
 
 	AppVer = 9
 )
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "0.2.0"
+	Version = "0.2.5"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
