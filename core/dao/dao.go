06c1a2838ff1933b57b2989d5393eccdbd56a6e5
i9616271555 <i9616271555@gmail.com>
2021-01-11 13:06:38 +0300

    simple DEV/DAO rewards

--- core/dao/dao.go
+++ core/dao/dao.go
@@ -1,15 +1,11 @@
 package dao
 
 import (
-	"github.com/noah-blockchain/noah-go-node/core/state/accounts"
 	"github.com/noah-blockchain/noah-go-node/core/types"
 )
 
+// Commission which is subtracted from rewards and being send to DAO Address
 var (
-	Address = (&accounts.Multisig{
-		Threshold: 2,
-		Weights:   []uint{1, 1, 1},
-		Addresses: []types.Address{types.HexToAddress("NOAHxf98017d1a37cc4bec05026ef94cb46102e16638e"), types.HexToAddress("NOAHxf98017d1a37cc4bec05026ef94cb46102e16638e"), types.HexToAddress("NOAHxf98017d1a37cc4bec05026ef94cb46102e16638e")},
-	}).Address()
-	Commission = 10
+	Address    = types.HexToAddress("NOAHxf98017d1a37cc4bec05026ef94cb46102e16638e")
+	Commission = 10 // in %
 )
